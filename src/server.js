51ccbb9e571c8f1cbc3dd547ed9c8ff2527dbdc6
jaseat <jaseat@users.noreply.github.com>
2018-06-21 19:56:30 -0700

    cleaned up code

--- src/server.js
+++ src/server.js
@@ -1,6 +1,23 @@
-const express = require('express');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var logger = require('morgan');
 
-const app = express();
-const port = process.env.PORT || 5000;
+var mongoose = require('./connection');
+var Article = require('./models/Articles');
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+var htmlRouter = require('./routes/html');
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger('tiny'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', htmlRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT);
